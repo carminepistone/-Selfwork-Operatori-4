cc061434cded1c742f5e50901ac043e2601a85a6
carminepistone <132765808+carminepistone@users.noreply.github.com>
2026-02-02 21:10:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-// Variabili 
-let numeroTotaleGatti = 53; 
-let numeroGattiPerFila = 8; 
 
-// Gatti rimasti fuori
-let gattiRimanenti = Math.ceil(numeroTotaleGatti % numeroGattiPerFila);
+let numTotaleGatti = 53; 
+let numGattiPerFila = 8; 
 
-// File complete
-let numeroFileComplete = Math.ceil(numeroTotaleGatti - gattiRimanenti) / numeroGattiPerFila;
+let numFile = Math.floor(numeroTotaleGatti / numeroGattiPerFila);
 
-// Gatti che servono per completare una nuova fila
-let gattiPerNuovaFila = (gattiRimanenti && numeroGattiPerFila - gattiRimanenti) || 0;
+let numRimastiFuori = numeroTotaleGatti % numGattiPerFila;
+console.log(numRimastiFuori);
+
+let gattiPerNuovaFila = numeroGattiPerFila - numRimastiFuori;
 
 // Risultati
-console.log(`Ci sono ${numeroFileComplete} file di gatti e ne mancano ${gattiPerNuovaFila} per una nuova fila, con un avanzo di ${gattiRimanenti}` );
+console.log(`Ci sono ${numFile} file di gatti e ne mancano ${gattiPerNuovaFila} per una nuova fila, con un avanzo di ${numRimastiFuori}` );
